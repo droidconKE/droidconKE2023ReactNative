d3f73f595235d388189bb0339710bb10fa9e0ff4
brianwachira <brianwachira7@gmail.com>
2023-05-31 13:02:21 +0300

    [fix] : add style props to textstyle

--- components/common/StyledText.tsx
+++ components/common/StyledText.tsx
@@ -1,12 +1,12 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useTheme } from '@react-navigation/native';
 import React from 'react';
+import type { StyleProp, TextStyle } from 'react-native';
 import { Text as NativeText } from 'react-native';
 import { typography } from '../../config/typography';
 
 type TextProps = {
   children: React.ReactNode;
-  style?: any;
+  style?: StyleProp<TextStyle>;
   small?: boolean;
   title?: boolean;
   bold?: boolean;
@@ -19,7 +19,7 @@ type TextProps = {
  * @param title: boolean - title font size 18px and bold
  * @param subtitle: boolean - subtitle font size 16px and semi-bold
  * @param bold: boolean - bold font
- * @param style: any - custom style
+ * @param style: StyleProp<TextStyle> - custom style
  * @param children: React.ReactNode - text
  */
 const StyledText = (props: TextProps & NativeText['props']) => {
