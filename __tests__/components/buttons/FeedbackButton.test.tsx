1d9fd23a2be890d5c1e02f170c905199033abdb0
brianwachira <brianwachira7@gmail.com>
2023-06-08 23:19:39 +0300

    [feat] : add test for feedbackButton

---
+++ __tests__/components/buttons/FeedbackButton.test.tsx
@@ -0,0 +1,20 @@
+import { fireEvent, render, screen } from '@testing-library/react-native';
+import React from 'react';
+import FeedbackButton from '../../../components/buttons/FeedbackButton';
+
+describe('<FeedbackButton/>', () => {
+  // Test the feedback button component.
+  it('Renders feedback button component', () => {
+    render(<FeedbackButton onPress={() => console.log('Feedback gotten succesfully')} />);
+    expect(screen.getByText('Feedback')).toBeDefined();
+  });
+
+  // Test that onPress function works
+  it('calls the function provided by onPress prop after pressing the button', () => {
+    const onPress = jest.fn();
+    render(<FeedbackButton onPress={onPress} />);
+    fireEvent.press(screen.getByText('Feedback'));
+
+    expect(onPress).toHaveBeenCalledTimes(1);
+  });
+});
