7e3990ff3d054b1bbaf2314cec5bbb13a71b5f88
brianwachira <brianwachira7@gmail.com>
2023-06-13 11:48:22 +0300

    [feat] : revert Row to how it was previously but with some default props tweaks

--- components/common/Row.tsx
+++ components/common/Row.tsx
@@ -1,22 +1,19 @@
 import React from 'react';
 import type { StyleProp, ViewStyle } from 'react-native';
-import { StyleSheet, View as NativeView } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 type RowProps = {
   children: React.ReactNode;
+  testID?: string | undefined;
   style?: StyleProp<ViewStyle>;
 };
 
-/**
- * @param style: StyleProp<ViewStyle> - custom style
- * @param children: React.ReactNode - text
- */
-
-const Row = (props: RowProps & NativeView['props']) => {
-  const { style } = props;
-  const viewStyle = StyleSheet.compose(styles.row, style);
-
-  return <NativeView testID="row" style={viewStyle} {...props} />;
+const Row = ({ children, style, testID = 'row' }: RowProps) => {
+  return (
+    <View testID={testID} style={StyleSheet.compose(styles.row, style)}>
+      {children}
+    </View>
+  );
 };
 
 export default Row;
