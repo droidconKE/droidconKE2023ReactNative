4c4ae60e13165672a29cd6b1cfa1c2d8f1cea085
Tony Kharioki <antolisah@gmail.com>
2023-07-01 00:47:56 +0300

    custom nute/unmute button for video

--- components/player/VideoPlayer.tsx
+++ components/player/VideoPlayer.tsx
@@ -1,17 +1,23 @@
 /* eslint-disable react/no-unstable-nested-components */
+import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '@react-navigation/native';
+import type { VideoProps } from 'expo-av';
 import { ResizeMode, Video } from 'expo-av';
 import { Image } from 'expo-image';
-import React, { useRef } from 'react';
-import { Dimensions, StyleSheet, View } from 'react-native';
+import React, { useRef, useState } from 'react';
+import { Dimensions, Pressable, StyleSheet, View } from 'react-native';
 import { VIDEO_URL } from '../../config/constants';
 
 const { width } = Dimensions.get('window');
 
+type VideoPlayerRef = Video | null;
+
 const VideoPlayer = () => {
+  const [status, setStatus] = useState({} as VideoProps['status']);
+
   const { colors } = useTheme();
 
-  const video = useRef(null);
+  const video = useRef<VideoPlayerRef>(null);
 
   return (
     <View style={[styles.container, { backgroundColor: colors.card }]} testID="video-player">
@@ -22,12 +28,13 @@ const VideoPlayer = () => {
         source={{
           uri: VIDEO_URL,
         }}
-        useNativeControls
+        useNativeControls={false}
         resizeMode={ResizeMode.COVER}
         isLooping
-        isMuted={false}
+        isMuted
         volume={0.3}
         shouldPlay
+        onPlaybackStatusUpdate={(_status) => setStatus(() => _status)}
         usePoster
         PosterComponent={() => (
           <Image
@@ -38,6 +45,17 @@ const VideoPlayer = () => {
           />
         )}
       />
+      {video.current && (
+        <View style={styles.button}>
+          <Pressable
+            onPress={() => {
+              status?.isMuted ? video.current?.setIsMutedAsync(false) : video.current?.setIsMutedAsync(true);
+            }}
+          >
+            <Ionicons name={status?.isMuted ? 'volume-mute-sharp' : 'volume-medium-sharp'} size={30} color="white" />
+          </Pressable>
+        </View>
+      )}
     </View>
   );
 };
@@ -61,5 +79,16 @@ const styles = StyleSheet.create({
     height: 180,
     width: '100%',
     borderRadius: 12,
+  },
+  button: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    backgroundColor: 'rgba(0,0,0,0.5)',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
