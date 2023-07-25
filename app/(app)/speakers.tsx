21b1aa82e28fcb27ec56728063b45cba15015e83
Tony Kharioki <antolisah@gmail.com>
2023-07-25 23:45:48 +0300

    Speakers Page and <SpeakerCard /> Fixes #23

--- app/(app)/speakers.tsx
+++ app/(app)/speakers.tsx
@@ -1,44 +1,69 @@
-import { Link, Stack } from 'expo-router';
-import React from 'react';
-import StyledText from '../../components/common/StyledText';
+import { AntDesign } from '@expo/vector-icons';
+import { useTheme } from '@react-navigation/native';
+import { Stack, useRouter } from 'expo-router';
+import React, { useEffect, useState } from 'react';
+import { FlatList, StyleSheet, View } from 'react-native';
+import SpeakerCard from '../../components/cards/SpeakerCard';
+import Space from '../../components/common/Space';
 import MainContainer from '../../components/container/MainContainer';
+import type { Session, Speaker } from '../../global';
+import { Sessions } from '../../mock/sessions';
+import { Speakers } from '../../mock/speakers';
 
-// TODO: implement speakers page
-/**
- * TASK:
- * - Should display a grid view of speakers
- * - <SpeakerCard /> should be used to display each speaker
- */
-// TODO: this is dummy data, replace with real data from mock/speakers.ts
-const _speakers = [
-  {
-    id: '1',
-    name: 'John Doe',
-  },
-  {
-    id: '2',
-    name: 'Jane Doe',
-  },
-];
+interface SpeakerItem extends Speaker {
+  sessions: Array<Session>;
+}
 
-const speakers = () => {
+const SpeakersPage = () => {
+  const { colors } = useTheme();
+  const router = useRouter();
+  const [data, setData] = useState<Array<SpeakerItem>>([]);
+
+  const getSpeakerSessions = () => {
+    const speakerArray = [...Speakers.data] as Array<SpeakerItem>;
+
+    speakerArray.map((speaker) => {
+      speaker.sessions = Sessions.data.filter((session) =>
+        session.speakers.some((_speaker) => _speaker.name === speaker.name),
+      );
+    });
+    setData(speakerArray);
+  };
+
+  useEffect(() => {
+    getSpeakerSessions();
+  }, []);
+
   return (
-    <MainContainer preset="scroll">
+    <MainContainer>
       <Stack.Screen
         options={{
           title: 'Speakers',
-          headerTitleAlign: 'center',
+          headerLeft: () => <AntDesign name="arrowleft" size={24} color={colors.text} onPress={() => router.back()} />,
         }}
       />
-      <StyledText>speakers</StyledText>
 
-      {_speakers.map((speaker) => (
-        <Link key={speaker.id} href={{ pathname: `${speaker.id}`, params: { id: speaker.id } }}>
-          <StyledText>Go to {speaker.name}'s page</StyledText>
-        </Link>
-      ))}
+      <View style={styles.main}>
+        <Space size={16} />
+
+        <FlatList
+          data={data}
+          numColumns={2}
+          renderItem={({ item }) => <SpeakerCard {...item} />}
+          keyExtractor={(_, index) => index.toString()}
+        />
+
+        <Space size={16} />
+      </View>
     </MainContainer>
   );
 };
 
-export default speakers;
+export default SpeakersPage;
+
+const styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    paddingHorizontal: 10,
+  },
+});
--- components/cards/SpeakerCard.tsx
+++ components/cards/SpeakerCard.tsx
@@ -1,14 +1,98 @@
+import { useTheme } from '@react-navigation/native';
+import { Image } from 'expo-image';
+import { Link, useRouter } from 'expo-router';
 import React from 'react';
-import { View } from 'react-native';
+import { Dimensions, StyleSheet, TouchableOpacity, View } from 'react-native';
+import type { Session, Speaker } from '../../global';
+import { truncate } from '../../util/helpers';
+import Space from '../common/Space';
+import StyledText from '../common/StyledText';
 
-// TODO: Implement speaker card.
-/**
- * - each speaker card should display Image of speaker, name, description, and session buttons
- * - clicking session button should navigate to [session] page
- */
+const { width } = Dimensions.get('window');
 
-const SpeakerCard = () => {
-  return <View />;
+interface SpeakerCardProps extends Speaker {
+  sessions: Array<Session>;
+}
+
+const SpeakerCard = (props: SpeakerCardProps) => {
+  const { colors } = useTheme();
+  const router = useRouter();
+
+  const { name, avatar, tagline, sessions } = props;
+
+  return (
+    <View style={[styles.card, { backgroundColor: colors.card }]}>
+      <Image source={{ uri: avatar }} style={[styles.image, { borderColor: colors.tint }]} contentFit="contain" />
+
+      <Space size={16} />
+
+      <Link href={{ pathname: `/${name}`, params: { name: name } }}>
+        <StyledText font="bold" size="base" variant="primary">
+          {name}
+        </StyledText>
+      </Link>
+
+      <Space size={8} />
+
+      <View style={styles.tagline}>
+        <StyledText size="sm" variant="text" style={{ color: colors.textLight }}>
+          {truncate(50, tagline)}
+        </StyledText>
+      </View>
+
+      <Space size={16} />
+
+      {sessions.length > 0 && (
+        <TouchableOpacity
+          style={[styles.button, { borderColor: colors.tint }]}
+          onPress={() =>
+            router.push({
+              pathname: `/session/${sessions[0]?.slug}`,
+              params: { slug: sessions[0]?.slug },
+            })
+          }
+          testID="sessionButton"
+        >
+          <StyledText size="base" font="semiBold" style={[styles.buttonText, { color: colors.tint }]}>
+            Session
+          </StyledText>
+        </TouchableOpacity>
+      )}
+    </View>
+  );
 };
 
 export default SpeakerCard;
+
+const styles = StyleSheet.create({
+  card: {
+    width: width / 2 - 24,
+    borderRadius: 10,
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    margin: 8,
+    alignItems: 'center',
+  },
+  image: {
+    width: 108,
+    height: 108,
+    borderRadius: 10,
+    borderWidth: 2,
+  },
+  tagline: {
+    width: '100%',
+    height: 60,
+    alignItems: 'center',
+  },
+  button: {
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 45,
+    borderRadius: 8,
+    borderWidth: 2,
+  },
+  buttonText: {
+    textTransform: 'uppercase',
+  },
+});
