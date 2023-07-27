9be86f4790eebdf3867a286f08ed7776705ff839
Tony Kharioki <antolisah@gmail.com>
2023-07-28 01:21:21 +0300

    Speakers Page and <SpeakerCard /> (#45)
    * Speakers Page and <SpeakerCard />
    Fixes #23
    
    * resolved centered text, abstracted function

--- components/cards/SpeakerCard.tsx
+++ components/cards/SpeakerCard.tsx
@@ -1,14 +1,113 @@
+import { useTheme } from '@react-navigation/native';
+import { Image } from 'expo-image';
+import { Link, useRouter } from 'expo-router';
 import React from 'react';
-import { View } from 'react-native';
+import { Dimensions, StyleSheet, TouchableOpacity, View } from 'react-native';
+import type { Session, Speaker } from '../../global';
+import { truncate } from '../../util/helpers';
+import Space from '../common/Space';
+import StyledText from '../common/StyledText';
 
-// TODO: Implement speaker card.
-/**
- * - each speaker card should display Image of speaker, name, description, and session buttons
- * - clicking session button should navigate to [session] page
- */
+const { width } = Dimensions.get('window');
 
-const SpeakerCard = () => {
-  return <View />;
+interface SpeakerCardProps extends Speaker {
+  sessions: Array<Session>;
+}
+
+const SpeakerCard = (props: SpeakerCardProps) => {
+  const { colors } = useTheme();
+  const router = useRouter();
+
+  const { name, avatar, tagline, sessions } = props;
+
+  return (
+    <View style={[styles.card, { backgroundColor: colors.card }]}>
+      <Image source={{ uri: avatar }} style={[styles.image, { borderColor: colors.tint }]} contentFit="contain" />
+
+      <Space size={16} />
+
+      <View style={styles.details}>
+        <Link href={{ pathname: `/${name}`, params: { name: name } }}>
+          <StyledText font="bold" size="base" variant="primary">
+            {name}
+          </StyledText>
+        </Link>
+
+        <Space size={8} />
+
+        <View style={styles.tagline}>
+          <StyledText size="sm" variant="text" style={[styles.text, { color: colors.textLight }]} numberOfLines={3}>
+            {truncate(60, tagline)}
+          </StyledText>
+        </View>
+
+        <Space size={16} />
+
+        {sessions.length > 0 && (
+          <TouchableOpacity
+            style={[styles.button, { borderColor: colors.tint }]}
+            onPress={() =>
+              router.push({
+                pathname: `/session/${sessions[0]?.slug}`,
+                params: { slug: sessions[0]?.slug },
+              })
+            }
+            testID="sessionButton"
+          >
+            <StyledText size="base" font="semiBold" style={[styles.buttonText, { color: colors.tint }]}>
+              Session
+            </StyledText>
+          </TouchableOpacity>
+        )}
+      </View>
+    </View>
+  );
 };
 
 export default SpeakerCard;
+
+const styles = StyleSheet.create({
+  card: {
+    width: width / 2 - 24,
+    borderRadius: 10,
+    paddingVertical: 16,
+    paddingHorizontal: 8,
+    margin: 8,
+    alignItems: 'center',
+  },
+  image: {
+    width: 108,
+    height: 108,
+    borderRadius: 10,
+    borderWidth: 2,
+  },
+  details: {
+    width: '100%',
+    flex: 1,
+    alignItems: 'center',
+  },
+  tagline: {
+    width: '100%',
+    flex: 1,
+    height: 50,
+  },
+  text: {
+    textAlign: 'center',
+  },
+  bottom: {
+    width: '100%',
+    alignItems: 'center',
+    flex: 1,
+  },
+  button: {
+    width: '90%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: 45,
+    borderRadius: 8,
+    borderWidth: 2,
+  },
+  buttonText: {
+    textTransform: 'uppercase',
+  },
+});
