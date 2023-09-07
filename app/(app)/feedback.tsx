b48e40c5accb437829e3c40b2f493ab74463d886
gmwaniki <gmang2236@gmail.com>
2023-09-07 12:22:59 +0200

    [fix] use correct padding size

--- app/(app)/feedback.tsx
+++ app/(app)/feedback.tsx
@@ -102,7 +102,7 @@ const styles = StyleSheet.create({
     flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-evenly',
-    paddingBottom: 57,
+    paddingBottom: 27,
   },
   FeedBackFormTitle: {
     textAlign: 'center',
