6f5ec9f6c731a5e6bebb4d021db9671d4a33f2ec
Tony Kharioki <antolisah@gmail.com>
2023-10-16 06:01:57 +0300

    removed avatar

--- __tests__/components/headers/HeaderRight.test.tsx
+++ __tests__/components/headers/HeaderRight.test.tsx
@@ -6,25 +6,10 @@ jest.mock('expo-router', () => ({
   useRouter: jest.fn(),
 }));
 
-// To be uncommented when auth flow development resumes
-/*
-describe('<HeaderRightWithAuth/>', () => {
-  it('renders sign-in button when user is not logged in', () => {
-    const handlePress = jest.fn();
-    const { getByTestId } = render(<HeaderRightWithAuth handlePress={handlePress} />);
-    const headerRightButton = getByTestId('headerRightWithUser');
-    expect(headerRightButton).toBeTruthy();
-    fireEvent.press(headerRightButton);
-    expect(handlePress).toHaveBeenCalled();
-  });
-});
-*/
-
 describe('<HeaderRight/>', () => {
   it('renders HeaderRight component', () => {
     render(<HeaderRight />);
     expect(screen.getByTestId('headerRight')).toBeDefined();
-    expect(screen.getByTestId('avatar')).toBeDefined();
     expect(screen.getByText('Feedback')).toBeDefined();
   });
 });
