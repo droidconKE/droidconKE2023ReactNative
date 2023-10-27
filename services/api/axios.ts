77ddbf34471218a0ad1f7d167fcef44b7c5eddd9
Tony Kharioki <antolisah@gmail.com>
2023-10-27 06:14:07 +0300

    env vars and updated contributing guidelines

--- services/api/axios.ts
+++ services/api/axios.ts
@@ -3,7 +3,6 @@ import axios from 'axios';
 export const axiosInstance = axios.create({
   baseURL: process.env.EXPO_PUBLIC_DEV_BASE_URL,
   headers: {
-    Authorization: `Bearer ${process.env.EXPO_PUBLIC_TOKEN}`,
     'Api-Authorization-Key': process.env.EXPO_PUBLIC_API_AUTHORIZATION_KEY,
     Accept: 'application/json',
   },
